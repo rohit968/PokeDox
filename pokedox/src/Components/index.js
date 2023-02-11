e347c681c45575646d42c7c01f8426d123d768be
Rohit Tiwari <rohit.tiwari968@gmail.com>
2023-02-11 09:32:18 +0530

    Files setup
    Files setup

---
+++ pokedox/src/Components/index.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import Navbar from './Navbar'
+
+import SearchPanel from './SearchPanel'
+import Footer from './Footer'
+
+export { Navbar, SearchPanel, Footer }
---
+++ pokedox/src/pages/Home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Navbar, SearchPanel, Footer } from '../Components/index';
+
+const Home = () => {
+  return (
+    <>
+      <p>Hello</p>
+      <Navbar />
+      <SearchPanel />
+      <Footer />
+    </>
+  )
+}
+
+export default Home
